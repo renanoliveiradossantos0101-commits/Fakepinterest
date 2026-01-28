e0fe4df98c3f560f8421bb0874c44d0ecade6eac
Renan <renanoliveiradossantos0101@gmail.com>
2026-01-27 22:56:36 -0300

    alterações para deploy

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
+
+local = False
+if local == True:
+    caminho = os.getenv("DATABASE_URL")
+else:
+    caminho = "sqlite:///comunidade.db"
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = caminho
 app.config["SECRET_KEY"] = "cbb76c358d4ede83459dd3f885154329"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
